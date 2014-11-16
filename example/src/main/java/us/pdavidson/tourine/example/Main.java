872752bd978f673496b7c12d25b045e9d057a58c
Peter Davidson <pdavidso@gmail.com>
2014-11-16 12:05:01 -0700

    Removed Console Reporter

--- example/src/main/java/us/pdavidson/tourine/example/Main.java
+++ example/src/main/java/us/pdavidson/tourine/example/Main.java
@@ -20,7 +20,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Main main = new Main();
-        main.registerConsoleReporter();
+//        main.registerConsoleReporter();
         main.registerTourineReporter();
         main.start();
 
